9efcb085910b874167ed66c0cfa83a239e9de2b9
cloudgatedevelopment <cloudgatedevelopment@gmail.com>
2021-02-18 13:26:28 -0500

    8

--- v4-s/demo1/src/js/layout/base/header-dropdowns.js
+++ v4-s/demo1/src/js/layout/base/header-dropdowns.js
@@ -13,6 +13,32 @@ var KTLayoutHeaderDropdowns = function() {
         });
     }
 
+
+
+    /**
+     * 
+     * 
+     	KTLayoutHeaderDropdowns.init("dropdown-head");
+		var el = KTLayoutHeaderDropdowns.getToggleElement();
+		var outerHeight = KTUtil.outerHeight(el);
+		var height = outerHeight - 30;
+		var isInit = false;
+		if (outerHeight > 0) {
+			$("#main-container").css("margin-top", height);
+		}
+		function hideShow() {
+			// var el=KTLayoutHeaderDropdowns.getToggleElement();
+			// var height=KTUtil.height(el);
+			if (isInit) {
+				$("#main-container").css("margin-top", height);
+				isInit = false;
+			} else {
+				$("#main-container").css("margin-top", "-30px");
+				isInit = true;
+			}
+			$("#dropdown-head").toggle();
+		}
+     */
     // Public methods
 	return {
 		init: function(id) {
